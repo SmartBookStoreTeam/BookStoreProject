b6908f3b3bb8520e1d0a5cd383c2781c4458c082
Ziad Fawzi <ziadmohammed2592004@gmail.com>
2026-01-13 14:09:28 +0200

    Enable Google login button in Register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-// import GoogleLoginButton from "../components/GoogleLoginButton";
+ import GoogleLoginButton from "../components/GoogleLoginButton";
 import {
   BookOpenIcon,
   EyeIcon,
